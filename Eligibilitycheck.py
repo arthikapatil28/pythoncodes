006d0c731ed63b4f76ed7c80c199cf2d1169db63
arthikapatil28 <arthikapatil28@gmail.com>
2025-07-21 11:33:04 +0530

    Eligibilitycheck.py

---
+++ Eligibilitycheck.py
@@ -0,0 +1,10 @@
+Height=int(input("Enter the Height: "))
+if Height>120:
+    print("Congrts you are eligible to ride")
+    AGE=int(input("Enter the AGE: "))
+    if AGE>18:
+        print("Cost of ride is 20rs")
+    else:
+        print("Cost of ride is 10rs")
+else:
+    print("Opps You cant take a ride")
